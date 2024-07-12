59e45b7c67567fe61faa61f4a9b673d6e95f1c2d
HarshaVardhanRajuK <rajharsha666@gmail.com>
2024-07-13 00:28:40 +0530

    fallback for full stack version

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+import { inject } from "@vercel/analytics"
 
 // request elements
 const loanInpEle = document.getElementById("appLoanAmount");
@@ -23,7 +24,10 @@ const ROIpmEle = document.getElementById("ROIpm");
 const totalInterestAmountEle = document.getElementById("totalInterestAmount");
 const totalRepaymentAmountEle = document.getElementById("totalRepaymentAmount");
 const broken = document.getElementById("broken");
+const spinner = document.querySelector(".span")
+const buttonText = document.querySelector(".button-text")
 
+const url = import.meta.env.VITE_URL;
 
 function onchangeRange(ele) {
   let irrVal = document.getElementById("irrVal");
@@ -38,7 +42,7 @@ function handleInputs() {
     return false;
   }
 
-  if (loanInpEle.value < 20000) {
+  if (loanInpEle.value < 20000 || loanInpEle.value > 1150000) {
     errEle.textContent = "* Enter Valid Number";
     errEle.style.display = "inline";
 
@@ -47,76 +51,138 @@ function handleInputs() {
   return true;
 }
 
-function indianFormat(num) {
-   let formattedNum = parseFloat(num).toFixed(3);
-    
-   let parts = formattedNum.split(".");
-   let integerPart = parts[0];
-   let decimalPart = parseInt(parts[1]) ? parts[1] : "";
+function indianFormat(num, dec=3) {
+  let formattedNum = parseFloat(num).toFixed(dec);
 
-   let lastThree = integerPart.slice(-3);
-   let otherNumbers = integerPart.slice(0, -3);
+  let parts = formattedNum.split(".");
+  let integerPart = parts[0];
+  let decimalPart = parseInt(parts[1]) ? parts[1] : "";
 
-   if (otherNumbers !== '') {
-       lastThree = ',' + lastThree;
-   }
-   let indianFormattedNumber = otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+  let lastThree = integerPart.slice(-3);
+  let otherNumbers = integerPart.slice(0, -3);
 
-   return decimalPart ? indianFormattedNumber + "." + decimalPart : indianFormattedNumber;
+  if (otherNumbers !== "") {
+    lastThree = "," + lastThree;
+  }
+  let indianFormattedNumber =
+    otherNumbers.replace(/\B(?=(\d{2})+(?!\d))/g, ",") + lastThree;
+
+  return decimalPart
+    ? indianFormattedNumber + "." + decimalPart
+    : indianFormattedNumber;
 }
 
-async function checkEmi() {
-  if (!handleInputs()) {
-    return;
-  }
-
+async function fetchFunc() {
   try {
+    buttonText.classList.add("hidden")
+    spinner.classList.remove("hidden")
+    const inputData = {
+      approvedLoanAmount: parseInt(loanInpEle.value),
+      lifeRadio: lifeRadio.checked,
+      healthRadio: healthRadio.checked,
+      fpr: fpr.checked,
+      irr: parseInt(irrEle.value),
+      tenure: parseInt(tenureEle.value)
+    };
+    
+    const cacheKey = JSON.stringify(inputData)
+    const cacheVersion = 'v1';
+    
+    let cachedData = localStorage.getItem(cacheKey);
+    
+    let data;
+    
+    if (cachedData) {
+      cachedData = JSON.parse(cachedData)
+      
+      if (cachedData.v === cacheVersion) {
+        data = cachedData.data;
+      }else {
+        localStorage.removeItem(cacheKey)
+        
+      }
+    }
+    
+    if (!data) {
+      
+      const options = {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: cacheKey,
+      };
 
-  const inputData = {
-    approvedLoanAmount: parseInt(loanInpEle.value),
-    lifeRadio: lifeRadio.checked,
-    healthRadio: healthRadio.checked,
-    fpr: fpr.checked,
-    irr: parseInt(irrEle.value),
-    tenure: parseInt(tenureEle.value),
+      const response = await fetch(url, options);
+      
+      data = await response.json();
+      
+      localStorage.setItem( cacheKey, JSON.stringify({data, v: "v1"}) )
+    }
+    
+    buttonText.classList.remove("hidden")
+    spinner.classList.add("hidden")
+
+    const {
+      lifeAmount,
+      healthAmount,
+      processingFee,
+      totalGrossAmount,
+      totalLoanAmount,
+      emi,
+      totalRepaymentAmount,
+      totalInterestAmount,
+      ROIpa,
+      ROIpm,
+      brokenCharges,
+    } = data;
+
+    lifeAmountEle.value = indianFormat(lifeAmount);
+    healthAmountEle.value = indianFormat(healthAmount);
+    proFee.value = indianFormat(processingFee);
+    grossAmount.value = indianFormat(totalGrossAmount);
+    totalLoanAmountEle.value = indianFormat(totalLoanAmount);
+    emiEle.value = indianFormat(emi);
+    ROIpaEle.value = indianFormat(ROIpa, 2);
+    ROIpmEle.value = indianFormat(ROIpm, 2);
+    totalRepaymentAmountEle.value = indianFormat(totalRepaymentAmount);
+    totalInterestAmountEle.value = indianFormat(totalInterestAmount);
+    broken.value = indianFormat(brokenCharges);
+  } catch (err) {
+    console.log(err);
   }
-
-  const options = {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify(inputData),
-    credentials: "include"
-  }
-
-  const response = await fetch("https://vercel-node-5wvxbvah4-harshavardhanrajuks-projects.vercel.app/calculate", options)
-
-  const data = await response.json()
-
-  const {lifeAmount, healthAmount, processingFee, totalGrossAmount, totalLoanAmount, emi, totalRepaymentAmount, totalInterestAmount, ROIpa, ROIpm, brokenCharges} = data
-
-  lifeAmountEle.value = indianFormat(lifeAmount);
-  healthAmountEle.value = indianFormat(healthAmount);
-  proFee.value = indianFormat(processingFee)
-  grossAmount.value = indianFormat(totalGrossAmount);
-  totalLoanAmountEle.value = indianFormat(totalLoanAmount);
-  emiEle.value = indianFormat(emi);
-  ROIpaEle.value = ROIpa
-  ROIpmEle.value = ROIpm
-  totalRepaymentAmountEle.value = indianFormat(totalRepaymentAmount);
-  totalInterestAmountEle.value = indianFormat(totalInterestAmount);
-  broken.value = indianFormat(brokenCharges)
-
-}catch(err){
-  console.log(err)
-}
 }
 
 function changeErrFunc() {
   document.getElementById("error").style.display = "none";
 }
 
+const debouncedClick = (e) => {
+
+  if (e.disabled) {
+    return;
+  }
+
+  if (!handleInputs()) {
+    return;
+  }
+
+  e.disabled = true;
+
+  setTimeout(() => {
+    e.disabled = false;
+  }, 800);
+
+  fetchFunc();
+};
+
+loanInpEle.addEventListener("keyup", (e)=>{
+  if(e.key === "Enter"){
+
+    debouncedClick(e)}
+
+})
+window.debouncedClick = debouncedClick;
 window.onchangeRange = onchangeRange;
 
 window.checkEmi = checkEmi;
@@ -124,3 +190,4 @@ window.checkEmi = checkEmi;
 window.handleInputs = handleInputs;
 window.changeErrFunc = changeErrFunc;
 
+inject({debug: false})
